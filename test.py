178604ba50b00db1c89be73a764d60a6ac82fdd6
ghostwang <mine4ghost@gmail.com>
2014-03-17 04:16:01 +0800

    ”更改成可配置代码格式的模版了“

--- test.py
+++ test.py
@@ -1,33 +1,38 @@
 import xml.etree.ElementTree as ET
+from string import Template
+code_temp = Template('$SUGAR private $TYPE $NAME ;\n')
+get_temp = Template('public $TYPE get$NAME \b(){\n\treturn this.$NAME \b;\n}\n')
+set_temp = Template('public void set$NAME \b($TYPE $NAME){\n\tthis.$NAME = $NAME;\n}\n')
+import_temp = Template("import $IMPORTNAME ;\n")
+class_temp = Template("public class $TABLENAME {\n $DECLARE $GETSETFUN \n}")
 class Elem:
-    elem_sugar = []
-    elem_type = ''
-    elem_name = ''
+    elem_dic = {}
 
     def __init__(self,su,ty,na):
-        self.elem_sugar = su
-        self.elem_type = ty
-        self.elem_name = na
+        sugar_temp = ""
+        for sugar in su:
+            sugar_temp+=sugar;
+            sugar_temp+="\n";
 
-    def print_elem(self):
-        for su in self.sugar:
-            print su
-        print "private "+self.elem_type +" "+self.elem_name + ";"
+        self.elem_dic=dict(SUGAR=sugar_temp,TYPE=ty,NAME=na)
+
+    def set_code_template(self,code_template):
+        code_temp = Template(code_template)
+
+    def set_set_fun_template(self,set_fun_template):
+        set_temp = Template(set_fun_template)
+
+    def set_get_fun_template(self,get_fun_template):
+        get_temp =Template(get_fun_template)
+
+
     def get_code(self):
-        code = ""
-        for su in self.elem_sugar:
-            code += su;
-            code += "\n"
-
-        code += "private " + self.elem_type +" "+self.elem_name + ";\n"
-        return code
+        return code_temp.substitute(self.elem_dic)
     def get_get_fun(self):
-        code = "public "+self.elem_type+" get"+self.elem_name+"(){\n\treturn this."+self.elem_name+";\n}\n"
-        return code
+        return get_temp.substitute(self.elem_dic)
 
     def get_set_fun(self):
-        code = "public void set"+self.elem_name+" ("+self.elem_type+" "+self.elem_name+"){\n\tthis."+self.elem_name+"="+self.elem_name+";\n}\n"
-        return code
+        return set_temp.substitute(self.elem_dic)
 
 class Table:
     table_name = ''
@@ -35,29 +40,41 @@ class Table:
     table_elem = []
     def __init__(self,tn):
         self.table_name=tn
+
+    
     def add_import(self, import_name):
         self.table_import.append(import_name)
 
     def add_elem(self, elem_name):
         self.table_elem.append(elem_name)
 
+
+    def set_import_template(self,template):
+        import_temp = Template(template)
+    
+    def set_class_template(self,template):
+        class_temp = Template(template)
+
     def get_entity_code(self):
         code = ""
         for imp in self.table_import:
-            code += "import "+imp+";\n"
-        code += "public class "+self.table_name+" {\n"
+            code += import_temp.substitute(IMPORTNAME=imp)
+        declare_code=""
         for elem in self.table_elem:
-            code += elem.get_code()
+            declare_code += elem.get_code()
+
+        getset_code =""
         
         for elem in self.table_elem:
-            code += elem.get_get_fun()
-            code += elem.get_set_fun()
+            getset_code += elem.get_get_fun()
+            getset_code += elem.get_set_fun()
 
-        code += "}"
+        code +=class_temp.substitute(TABLENAME=self.table_name,DECLARE=declare_code, GETSETFUN=getset_code)
 
         return code
 
 def init():
+    set_template("code.tmp")
     tree = ET.parse("table.xml")
     root = tree.getroot()
     table = Table(root.attrib['name'])
@@ -72,7 +89,19 @@ def init():
             su.append(su_elem.text)
         table.add_elem(Elem(su,elem.find("field/type").text,elem.find("field/name").text))
 
+    
+
     print table.get_entity_code()
+
+def set_template(filename):
+    settings = open(filename,'r').readlines()
+    for line in settings:
+        if line.split():
+            k,v = line.split(":")
+            k=k.strip()
+            v=v.strip()
+            exec(k+" = Template("+v+")")
+
 
 if __name__ == '__main__':
     init()
