aa96c0fcb6609e0fc15b60f36bd637c3a0000807
ghostwang <mine4ghost@gmail.com>
2014-03-18 02:08:26 +0800

    变成固定模版了，利用正则表达式进行替换

---
+++ code.tpl
@@ -0,0 +1,26 @@
+import java.XXX.x;
+import javax.xxx.x;
+public class $TABLENAME {
+    <code>
+        @column
+        @id
+        private String $NAME;
+        public String get$NAME() {
+            return this.$NAME;
+        }
+        public String set$NAME(String $NAME){
+            this.$NAME = $NAME;
+        }
+    </code>
+    <code>
+        @column
+        @id
+        private int $NAME;
+        public int get$NAME() {
+            return this.$NAME;
+        }
+        public int set$NAME(String $NAME){
+            this.$NAME = $NAME;
+        } 
+    </code>
+}
---
+++ test2.py
@@ -0,0 +1,39 @@
+import re
+import xml.etree.ElementTree as ET
+
+
+def init():
+    text = open("./code.tpl","r").read()
+    pattern = re.compile(u"<code>([\s\S]*?)</code>")
+    match = pattern.findall(text)
+    dic = {}
+    # "String"->"@column @id private String $NAME; \n public String get$NAME { return this.$NAME;}\public void set$NAME (String $NAME) {this.$NAME = $NAME;}"
+    for mat in match:
+        pattern = re.compile(r"private[\s]+(\w+)")
+        tpl_type = pattern.findall(mat)[0]
+        dic[tpl_type]=mat
+
+    tree = ET.parse("table.xml")
+    root = tree.getroot()
+    elem_code = ""
+    #$TABLENAME = root.attrib['name']
+    table_name = root.attrib['name']
+    imports = root.findall("import/item") #import string list
+
+    elems = root.findall("column")
+    for elem in elems:
+        code_temp = ""
+        elem_type = elem.find("field/type").text
+        elem_name = elem.find("field/name").text
+        code_temp += dic[elem_type]
+        elem_code += code_temp.replace(r'$NAME',elem_name)
+
+    final_code= re.compile(u'<code>[\s\S]*<\/code>').sub(elem_code,text)
+    final_code = final_code.replace(r'$TABLENAME',table_name)
+    print final_code
+
+if __name__ == "__main__":
+    init()
+        
+        
+
